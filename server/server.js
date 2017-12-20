eacd30983d4c39284266d66b7068637a523dc738
meatball <meatball@meatballs-MacBook-Pro.local>
2017-12-20 09:17:06 -0800

    socket.io added

--- server/server.js
+++ server/server.js
@@ -1,16 +1,27 @@
 const path = require('path');
+const http = require('http');
 const publicPath = path.join(__dirname, '../public');
 const express = require('express');
+const socketIO = require('socket.io');
+
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-console.log(__dirname + '/../public');
-console.log(publicPath);
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+});
 
 
-app.listen(port, () => {
+
+server.listen(port, () => {
   console.log(`server started up at port ${port}`);
 });
